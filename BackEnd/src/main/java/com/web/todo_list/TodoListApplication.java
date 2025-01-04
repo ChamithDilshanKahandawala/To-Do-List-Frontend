6b47d86b9b6dead74c6ea4c5cfb3098edc44f958
K.M.Chamith Dilshan <dilshankmc91@gmail.com>
2025-01-04 16:04:53 +0530

    Initial Commit for the frontend with js

---
+++ BackEnd/src/main/java/com/web/todo_list/TodoListApplication.java
@@ -0,0 +1,13 @@
+package com.web.todo_list;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoListApplication.class, args);
+	}
+
+}
